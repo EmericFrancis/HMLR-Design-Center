99701a45c681fbe9888740d90b31d71438cb6749
Emeric Francis <Emeric.Francis@landregistry.gov.uk>
2018-08-08 09:36:17 +0100

    adding the java script and css for accordions into the prototype

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,21 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+//for accordions
+if (
+  'addEventListener' in document &&
+  document.querySelectorAll
+  ) {
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+    var accordions = document.querySelectorAll('.accordion')
+
+    for (var i = accordions.length - 1; i >= 0; i--) {
+      new Accordion(accordions[i])
+    };
+
+  })
+
+}
